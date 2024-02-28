1c83f6a42a504e2a822e5b2e171770c6c3d7bd34
microwave789 <merei.zhazitova@gmail.com>
2024-02-28 18:19:34 +0600

    Create app layout

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "homework_n1"
+include(":app")
+ 
